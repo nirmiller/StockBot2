059243d4d235ea56de4cb3515d5a09d45088a91b
nirmiller <52181530+nirmiller@users.noreply.github.com>
2021-10-18 23:21:38 -0500

    d

--- testing_2.py
+++ testing_2.py
@@ -3,7 +3,7 @@ from keras.models import load_model
 import os
 from handling_2 import *
 
-stock_name, model_name = "PLUG", "/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_3/model_3_1_90"
+stock_name, model_name = "PLUG", "/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_3/model_3_2_20"
 model = load_model(model_name)
 window_size = model.layers[0].input.shape.as_list()[1]
 
