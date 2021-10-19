65a7979a8a890efc34aa6398de9a81d3d888680d
nirmiller <52181530+nirmiller@users.noreply.github.com>
2021-10-18 23:20:35 -0500

    d

--- training_2.py
+++ training_2.py
@@ -130,7 +130,7 @@ for e in range(episode_count + 1):
             print("REPLAY {}".format(agent.epsilon))
 
     if e % 10 == 0:
-        agent.model.save("/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_3/model_3_2_{}".format(str(e)))
+        agent.model.save("/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_3/model_3_3_{}".format(str(e)))
 
     if e % 7 == 0:
         agent.epsilon = 0.5
