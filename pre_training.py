5cb81c5866bc7ac12bb4cb42fafcbb40f437c7a3
nirmiller <nir.miller928@gmail.com>
2021-12-30 14:21:58 -0600

    hrkk

--- pre_training.py
+++ pre_training.py
@@ -17,5 +17,7 @@ l = len(data[0])
 
 for i in range(TIME_RANGE, l):
     snap = getState(data, 1, i, TIME_RANGE, PRICE_RANGE)
+    x_data.append(snap)
 
-print(snap)
+x_data = np.array(fix_input(x_data))
+print(x_data)
