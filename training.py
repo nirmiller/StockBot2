a5a8fce1dd9473df2741342575c4bc2a83c8d440
nirmiller <nir.miller928@gmail.com>
2022-01-02 07:35:10 -0600

    hrkk

--- training.py
+++ training.py
@@ -122,7 +122,7 @@ for e in range(episode_count + 1):
             print("REPLAY {}".format(agent.epsilon))
 
     if e % 5 == 0:
-        agent.model.save("/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_5/model_5_2_{}".format(str(e)))
+        agent.model.save("/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_5/model_5_3_{}".format(str(e)))
 
     if e % 5 == 0:
         agent.epsilon = 0.45
