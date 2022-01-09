f65981b762f5702539880724302a81fd89f97a7a
nirmiller <nir.miller928@gmail.com>
2022-01-09 03:21:02 -0600

    hrkk

--- training_modified.py
+++ training_modified.py
@@ -124,8 +124,8 @@ for e in range(episode_count + 1):
             agent.expReplay(batch_size)
             print("REPLAY {}".format(agent.epsilon))
 
-    if e % 5 == 0:
-        agent.model.save("/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_7/model_7_4_{}".format(str(e)))
+    if e % 10 == 0:
+        agent.model.save("/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_7/model_7_5_{}".format(str(e)))
 
     if e % 5 == 0:
         agent.epsilon = 0.4
