e19777a0c064230dbd68a025cd4738f01c04c64c
nirmiller <nir.miller928@gmail.com>
2022-01-15 09:22:06 -0600

    hrkk

--- minute_handling_windowed.py
+++ minute_handling_windowed.py
@@ -174,7 +174,7 @@ class Agent:
         self.is_eval = is_eval
         self.total_inventory = []
 
-        self.gamma = 0.7
+        self.gamma = 0.96
         self.epsilon = .9
         self.epsilon_min = 0
         self.epsilon_decay = 0.995
