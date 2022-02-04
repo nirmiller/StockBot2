6c32aa2420496e0f81ace98627efc04c78e03a8a
nirmiller <nir.miller928@gmail.com>
2022-02-04 07:29:06 -0600

    hrkk

--- minute_handling.py
+++ minute_handling.py
@@ -191,9 +191,9 @@ class Agent:
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape_1))
         model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-        model.add(Conv2D(64, kernel_size=(1, 1), activation='relu'))
+        model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
         model.add(Flatten())
-        model.add(Dense(120, activation='relu'))
+        model.add(Dense(300, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
 
         model.compile(loss='mse', optimizer=Adam(learning_rate=.0001), metrics=['accuracy'])
