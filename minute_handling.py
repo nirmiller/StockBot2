628291cf3c9803a0df26c124c14d35ffc0f71a38
nirmiller <nir.miller928@gmail.com>
2022-02-04 13:02:11 -0600

    hrkk

--- minute_handling.py
+++ minute_handling.py
@@ -224,6 +224,6 @@ class Agent:
             target_f = self.model.predict(state)
             target_f[0][action] = target
 
-            self.model.fit(state, target_f, epochs=1, verbose=0)
+            self.model.fit(state, target_f, epochs=1)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
