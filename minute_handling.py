9bd8cfceb044bd8f444dc3b861cc2b50d9491239
nirmiller <nir.miller928@gmail.com>
2022-02-05 03:57:05 -0600

    hrkk

--- minute_handling.py
+++ minute_handling.py
@@ -173,7 +173,7 @@ class Agent:
         self.is_eval = is_eval
         self.total_inventory = []
 
-        self.gamma = 0.95
+        self.gamma = 0.75
         self.epsilon = .9
         self.epsilon_min = 0
         self.epsilon_decay = 0.995
